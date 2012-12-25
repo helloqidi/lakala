2359b4dc2716885783126525d6ee4e898ad33782
helloqidi <helloqidi@gmail.com>
2012-12-25 18:14:13 +0800

    add included method for core module

--- lib/lakala.rb
+++ lib/lakala.rb
@@ -1,3 +1,6 @@
 # encoding: utf-8
 require "lakala/version"
 require "lakala/lakala"
+require "lakala/core"
+require "lakala/config"
+require "lakala/client"
--- lib/lakala/core.rb
+++ lib/lakala/core.rb
@@ -2,8 +2,13 @@
 module Lakala
   # Core
   module ClassUtilMixin
+
+    def self.included(base) 
+      base.send :extend, ClassMethods
+      base.send :include, InstanceMethods
+    end
+
     module ClassMethods
-      
     end
     
     module InstanceMethods
@@ -16,7 +21,6 @@ module Lakala
             others[key] = val
           end
         end
-        self.send("raw=",params)
         self.send("#{:other_attrs}=", others) if self.respond_to? :other_attrs and others.size > 0
         self.send(:init) if self.respond_to? :init
       end        
